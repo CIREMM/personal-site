e893537d6e05c92ed39f9be20b2904b53c7915d9
Maingi <109253018+CIREMM@users.noreply.github.com>
2026-01-30 16:54:00 +0300

    Delete personal-site directory

--- personal-site/js/reveal.js
+++
@@ -1,7 +0,0 @@
-const els = document.querySelectorAll(".reveal");
-const io = new IntersectionObserver((entries) => {
-  for (const e of entries) {
-    if (e.isIntersecting) e.target.classList.add("show");
-  }
-}, { threshold: 0.12 });
-els.forEach(el => io.observe(el));
